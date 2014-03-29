85839306e9e2977152f84f01dc8e9915004eff0e
SijieTian <danielten08@gmail.com>
2014-03-29 10:33:26 -0400

    Fire object can lit

--- game jam/Assets/Scripts/FireObjScript.cs
+++ game jam/Assets/Scripts/FireObjScript.cs
@@ -3,14 +3,29 @@ using System.Collections;
 
 public class FireObjScript : MonoBehaviour {
 
+
+	public GameObject smallFire;
+	public float fireTime = 5f;
+
+	private bool isLit = false;
+	private GameObject insSmallFire;
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-	void OnCollisionEnter(Collision collision)
-	{
-		Collider collider = collision.collider;
+
+	void OnParticleCollision(GameObject other)
+	{		
+		if(other.tag == "gunfire")
+		{
+			print ("fire up");
+			if(!isLit){
+				insSmallFire = Instantiate(smallFire, this.transform.position, Quaternion.identity) as GameObject;
+				Destroy(insSmallFire, fireTime);
+				isLit = true;
+			}
+		}
 	}
 
 
