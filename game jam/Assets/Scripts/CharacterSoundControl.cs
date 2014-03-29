33e1456694846c1c9ed3860d50367afe0eaac8b7
SijieTian <danielten08@gmail.com>
2014-03-29 13:56:24 -0400

    Add sound effect of character

---
+++ game jam/Assets/Scripts/CharacterSoundControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSoundControl : MonoBehaviour {
+	
+	public AudioSource soundPlayer;
+	private bool isStepSoundPlay;
+	// Use this for initialization
+	void Start () {
+		soundPlayer = this.GetComponent<AudioSource>() as AudioSource;
+		isStepSoundPlay = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		if(directionVector != Vector3.zero)
+		{
+			if(!isStepSoundPlay){
+				soundPlayer.Play();
+				soundPlayer.loop = true;
+				isStepSoundPlay = true;
+			}
+		}
+		else{
+			soundPlayer.Stop();
+			isStepSoundPlay = false;
+		}
+	}
+}
