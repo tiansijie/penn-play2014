8f2e9e5aba5d8e45d6b21bd2190723adc02945f1
unknown <winifred-shao@Hotmail.com>
2014-03-30 03:10:46 -0400

    merge

--- game jam/Assets/Scripts/GhostShadowScript.cs
+++ game jam/Assets/Scripts/GhostShadowScript.cs
@@ -6,14 +6,14 @@ public class GhostShadowScript : MonoBehaviour {
 	private Vector3 parentLocation;
 	private Vector3 ranDirection;
 	private bool isActivate = false;
-	public GUIStyle startButton;
+
 	public float shadowSpeed = 1f;
 	// Use this for initialization
 	void Start () {
-		parentLocation = this.transform.parent.transform.position;
-		Vector3 objPos = new Vector3(parentLocation.x, 0.1f, parentLocation.z);
-		this.transform.position = objPos;
-		getRandomDirection();
+//		parentLocation = this.transform.parent.transform.position;
+//		Vector3 objPos = new Vector3(parentLocation.x, 0.1f, parentLocation.z);
+//		this.transform.position = objPos;
+//		getRandomDirection();
 	}
 
 	public void setActivate(bool active)
@@ -30,33 +30,33 @@ public class GhostShadowScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(isActivate){
-			this.renderer.enabled = true;
-			Vector3 currPos = transform.position;
-			currPos += ranDirection * Time.deltaTime * shadowSpeed;
-			Vector3 scaleGhost = new Vector3(1f, 
-			                                 Mathf.Abs(currPos.y - parentLocation.y) + 1f,	
-			                                 1f);
-			this.transform.localScale = scaleGhost;
-			this.transform.position = currPos;
-			Vector3 distoParent = new Vector3(currPos.x - parentLocation.x, 0f, currPos.z - parentLocation.z);
-			if(distoParent.magnitude > 3)
-				ranDirection = -ranDirection;
-			else if(distoParent.magnitude < 0.2){
-				getRandomDirection();
-			}
-		}
-		else{
-			//this.enabled = false;
-			this.renderer.enabled = false;
-		}
+//		if(isActivate){
+//			this.renderer.enabled = true;
+//			Vector3 currPos = transform.position;
+//			currPos += ranDirection * Time.deltaTime * shadowSpeed;
+//			Vector3 scaleGhost = new Vector3(1f, 
+//			                                 Mathf.Abs(currPos.y - parentLocation.y) + 1f,	
+//			                                 1f);
+//			this.transform.localScale = scaleGhost;
+//			this.transform.position = currPos;
+//			Vector3 distoParent = new Vector3(currPos.x - parentLocation.x, 0f, currPos.z - parentLocation.z);
+//			if(distoParent.magnitude > 3)
+//				ranDirection = -ranDirection;
+//			else if(distoParent.magnitude < 0.2){
+//				getRandomDirection();
+//			}
+//		}
+//		else{
+//			//this.enabled = false;
+//			this.renderer.enabled = false;
+//		}
 
 	}
-	void OnGUI()
-	{
-		if(GUI.Button(new Rect(500,500,384,166),"",startButton))
-		{
-			print ("aaa");
-		}
-	}
+//	void OnGUI()
+//	{
+//		if(GUI.Button(new Rect(500,500,384,166),"",startButton))
+//		{
+//			print ("aaa");
+//		}
+//	}
 }
