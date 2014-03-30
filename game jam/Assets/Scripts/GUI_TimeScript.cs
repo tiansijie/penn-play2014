795daba95edb82b6d2ea98e4163f37a8c6f79b8d
madongqing <madongqing@madongqings-MacBook-Air.local>
2014-03-30 00:38:15 -0400

    Time Display

---
+++ game jam/Assets/Scripts/GUI_TimeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_TimeScript : MonoBehaviour {
+	public GlobalScript gScript;
+	// Use this for initialization
+	void Start () {
+		gScript = GameObject.Find ("Global").GetComponent<GlobalScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.guiText.text = "Time: " +((int)gScript.gameTime).ToString ();
+	}
+}
---
+++ game jam/Assets/Scripts/GlobalScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalScript : MonoBehaviour {
+	public float gameTime;
+	// Use this for initialization
+	void Start () {
+		gameTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameTime += Time.deltaTime;
+	}
+}
